4a4d8d28d95f0f628d9020b16f775da557704fc9
TheWolfAround <thewolfaround@gmail.com>
2025-02-18 17:35:55 +0300

    improvements

--- gui.sh
+++ gui.sh
@@ -1,51 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Set the ANSI escape sequence for yellow text
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 # Set the ANSI escape sequence to reset text color
 RESET='\033[0m'
 
-# List of packages to check
-packages="python3-pip python3-dev python3-venv python3-tk"
-
-update_package_index=0
-# Loop through each package and check if it is installed
-for pkg in $packages; do
-    if dpkg -s "$pkg" 1> /dev/null; then
-        echo -e "${GREEN}$pkg is already installed.${RESET}"
-    else
-        if [ $update_package_index -eq 0 ]; then
-            sudo apt update
-            update_package_index=1 #the script will update the package index once
-        fi
-        echo -e "${YELLOW}$pkg is not installed. It will be installed via 'apt'${RESET}"
-        sudo apt install $pkg -y
-    fi
-done
-
-# This gets the directory the script is run from so pathing can work relative to the script where needed.
-SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
-
-# Step into GUI local directory
-cd "$SCRIPT_DIR" || exit 1
-
 if [ -z "$PREFERRED_PYTHON" ]; then
     PREFERRED_PYTHON="python3"
-fi
-
-if [ -d "$SCRIPT_DIR/virt" ]; then
-    source "$SCRIPT_DIR/virt/bin/activate"
-else
-    echo "Python Virtual Environment folder does not exist. Creating."
-    "$PREFERRED_PYTHON" -m venv "$SCRIPT_DIR/virt"
-    source "$SCRIPT_DIR/virt/bin/activate"
-fi
-
-if [[ -z "$VIRTUAL_ENV" ]]; then
-    echo "No Python virtual environment is activated."
-else
-    echo "Python virtual environment is activated: $VIRTUAL_ENV"
 fi
 
 # Get the Python version and extract the major, minor, and patch numbers
@@ -56,15 +19,29 @@ patch=$(echo $python_version | cut -d'.' -f3)
 
 # Check if the Python version is within the desired range
 if [[ $major -eq 3 && $minor -ge 11 && $minor -lt 13 ]]; then
-    echo "Python version $python_version is within the acceptable range."
+    echo -e "${GREEN}Python version $python_version is within the acceptable range.${RESET}"
 else
-    echo "Error: Python version $python_version is not within the acceptable range (3.11.0 to 3.13.0)."
+    echo -e "${RED}Error: Python version $python_version is not within the acceptable range (3.11.0 to 3.13.0).${RESET}"
     exit 1
 fi
 
-REQUIREMENTS_FILE="$SCRIPT_DIR/requirements_linux.txt"
+# This gets the directory the script is run from so pathing can work relative to the script where needed.
+SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
 
-# pip3 install -r "$REQUIREMENTS_FILE"
+# Step into GUI local directory
+cd "$SCRIPT_DIR" || exit 1
+
+if [[ -d "$SCRIPT_DIR/virt" ]]; then
+    source "$SCRIPT_DIR/virt/bin/activate"
+    echo -e "${GREEN}Python Virtual Environment activated.${RESET}"
+else
+    echo -e "${YELLOW}Python Virtual Environment folder does not exist.${RESET}"
+    echo -e "${YELLOW}Creating a Python Virtual Environment.${RESET}"
+    "$PREFERRED_PYTHON" -m venv "$SCRIPT_DIR/virt"
+    echo -e "${GREEN}Python Virtual Environment created.${RESET}"
+    source "$SCRIPT_DIR/virt/bin/activate"
+    echo -e "${GREEN}Python Virtual Environment activated.${RESET}"
+fi
 
 # Check if LD_LIBRARY_PATH environment variable exists
 if [[ -z "${LD_LIBRARY_PATH}" ]]; then
